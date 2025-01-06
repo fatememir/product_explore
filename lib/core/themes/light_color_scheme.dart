e7793e8616ff73b49bb45b4ece3cd47e982b9a9b
Fatemeh Mirzaei <f.mirzaei@ip-jam.com>
2025-01-06 10:05:57 +0330

    add them folder for app

---
+++ lib/core/themes/light_color_scheme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF006E14),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFF8AFB81),
+  onPrimaryContainer: Color(0xFF002202),
+  secondary: Color(0xFF53634E),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFD6E8CE),
+  onSecondaryContainer: Color(0xFF111F0F),
+  tertiary: Color(0xFF38656A),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFBCEBF0),
+  onTertiaryContainer: Color(0xFF002022),
+  error: Color(0xFFBA1A1A),
+  errorContainer: Color(0xFFFFDAD6),
+  onError: Color(0xFFFFFFFF),
+  onErrorContainer: Color(0xFF410002),
+  background: Color(0xFFFCFDF6),
+  onBackground: Color(0xFF1A1C19),
+  surface: Color(0xFFFCFDF6),
+  onSurface: Color(0xFF1A1C19),
+  surfaceVariant: Color(0xFFDEE4D8),
+  onSurfaceVariant: Color(0xFF42493F),
+  outline: Color(0xFF73796F),
+  onInverseSurface: Color(0xFFF1F1EB),
+  inverseSurface: Color(0xFF2F312D),
+  inversePrimary: Color(0xFF6FDE68),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF006E14),
+  outlineVariant: Color(0xFFC2C8BC),
+  scrim: Color(0xFF000000),
+);
+
+
